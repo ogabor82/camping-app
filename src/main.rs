3b31e5dbe1bd553530b809003f9fa5694003226e
Ocskó Gábor <gabor@proceve.com>
2024-07-17 22:08:18 +0200

    types and constants

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let mut destination= "Crater Lake";
+    let mut destination: &str  = "Crater Lake";
     println!("Destination: {}", destination);
     destination = "Sparks Lake";
     println!("Destination: {}", destination);
+
+    const DESTINATION: &str = "Crater Lake";
+    println!("Destination: {}", DESTINATION);
 }
